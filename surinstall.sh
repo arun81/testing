b927a93d8e29b20c13b19b7919babe0fd9107be7
arun81 <81.arun@gmail.com>
2019-01-19 15:41:47 +0530

    Update surinstall.sh

--- surinstall.sh
+++ surinstall.sh
@@ -1,3 +1,8 @@
+apt-get install libpcre3 libpcre3-dbg libpcre3-dev build-essential libpcap-dev   \
+                libnet1-dev libyaml-0-2 libyaml-dev pkg-config zlib1g zlib1g-dev \
+                libcap-ng-dev libcap-ng0 make libmagic-dev libjansson-dev        \
+                libnss3-dev libgeoip-dev liblua5.1-dev libhiredis-dev libevent-dev \
+                python-yaml rustc cargo
 echo "deb http://http.debian.net/debian jessie-backports main" > \
     /etc/apt/sources.list.d/backports.list
 apt-get update
