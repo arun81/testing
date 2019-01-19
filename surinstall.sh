09651ab10347beeebd6d6985aaecc6ca6fe5e3d9
arun81 <81.arun@gmail.com>
2019-01-19 15:36:47 +0530

    Update surinstall.sh

--- surinstall.sh
+++ surinstall.sh
@@ -1,3 +1,4 @@
-yum install epel-release
-yum install suricata
-
+echo "deb http://http.debian.net/debian jessie-backports main" > \
+    /etc/apt/sources.list.d/backports.list
+apt-get update
+apt-get install suricata -t jessie-backports
