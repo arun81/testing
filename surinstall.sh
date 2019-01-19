ccb917a46390217702e9c859d2ce753e60ad4303
arun81 <81.arun@gmail.com>
2019-01-19 14:50:19 +0530

    Update surinstall.sh

--- surinstall.sh
+++ surinstall.sh
@@ -1,10 +1,2 @@
-apt-get update && \
-apt-get install -y --no-install-recommends \ 
-   software-properties-common && \
-apt-add-repository ppa:oisf/suricata-stable && \
-apt-get update && \
-apt-get install -y --no-install-recommends \
-suricata && \
-apt-get purge -y --autoremove \
-software-properties-common && \
-apt-get clean
+add-apt-repository ppa:oisf/suricata-stable
+apt-get update
