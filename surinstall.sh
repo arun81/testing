9a782260bafbf790ab6a386f6d95979c023c59e8
arun81 <81.arun@gmail.com>
2019-01-19 15:26:05 +0530

    Update surinstall.sh

--- surinstall.sh
+++ surinstall.sh
@@ -1,7 +1,3 @@
-apt-get update 
-apt-get install -y --no-install-recommends software-properties-common 
-apt-add-repository ppa:oisf/suricata-stable 
-apt-get update 
-apt-get install -y --no-install-recommends suricata 
-apt-get purge -y --autoremove software-properties-common 
-apt-get clean
+yum install epel-release
+yum install suricata
+
