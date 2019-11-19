8e41ebade13df11bc697861a04e45fe62e61f950
arun81 <81.arun@gmail.com>
2019-11-19 16:48:59 +0530

    Update and rename test.txt to Elk_Stack.sh

--- Elk_Stack.sh
+++ Elk_Stack.sh
@@ -1,32 +1,32 @@
-elk-filebeat
-basic elk + filebeat setup on ubuntu
+# elk-filebeat
+# basic elk + filebeat setup on ubuntu
 
-install elasticsearch
+#install elasticsearch
 wget https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-7.4.2-amd64.deb
 wget https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-7.4.2-amd64.deb.sha512
 shasum -a 512 -c elasticsearch-7.4.2-amd64.deb.sha512
 sudo dpkg -i elasticsearch-7.4.2-amd64.deb
 sudo /bin/systemctl daemon-reload
 sudo /bin/systemctl enable elasticsearch.service
-To start:
+# To start:
 sudo systemctl start elasticsearch.service
-To stop:
+# To stop:
 sudo systemctl stop elasticsearch.service
-To check if elasticsearch is running:
+# To check if elasticsearch is running:
 curl -X GET "localhost:9200/?pretty"
-install kibana
+# install kibana
 wget https://artifacts.elastic.co/downloads/kibana/kibana-7.4.2-amd64.deb
 shasum -a 512 kibana-7.4.2-amd64.deb 
 sudo dpkg -i kibana-7.4.2-amd64.deb
 sudo /bin/systemctl daemon-reload
 sudo /bin/systemctl enable kibana.service
-To start:
+#To start:
 sudo systemctl start kibana.service
 
-To stop:
+#To stop:
 sudo systemctl stop kibana.service
 
-config file:
+# config file:
 /etc/kibana/kibana.yml
 install logstash
 wget https://artifacts.elastic.co/downloads/logstash/logstash-7.4.2-amd64.deb
