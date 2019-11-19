59ff4260d4e3b0b61e9bc0cad30a315efc218d7d
arun81 <81.arun@gmail.com>
2019-11-19 19:56:04 +0530

    Update Elk_Stack.sh

--- Elk_Stack.sh
+++ Elk_Stack.sh
@@ -35,7 +35,7 @@ sudo dpkg -i logstash-7.4.2-amd64.deb
 sudo /bin/systemctl daemon-reload
 sudo /bin/systemctl enable logstash.service
 sudo systemctl start logstash.service
-install filebeat
+# install filebeat
 curl -L -O https://artifacts.elastic.co/downloads/beats/filebeat/filebeat-7.4.2-amd64.deb
 sudo dpkg -i filebeat-7.4.2-amd64.deb
 sudo /bin/systemctl daemon-reload
